4e649528fc64111c65bffc17114ac7b686805c41
Agrim Shukla <andhakaaramay@gmail.com>
2025-05-04 17:48:19 +0530

    first class for my next js

---
+++ src/app/helloParagrph/page.tsx
@@ -0,0 +1,7 @@
+export default function Paragraph() {
+  return(
+    <p className="text-3xl  text-center mt-10">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+    </p>
+  )
+}
