c51b1a84f2b0b27a7938d97aa47de469221f4066
Agrim Shukla <andhakaaramay@gmail.com>
2025-05-04 19:18:05 +0530

    dark and light theme

--- src/app/helloParagrph/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Paragraph() {
-  return(
-    <p className="text-3xl  text-center mt-10">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-    </p>
-  )
-}
